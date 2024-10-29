86a3b041a3214fff951433c1b6fc7a587738251b
Dorien Huijser <dorienhuijser@outlook.com>
2024-10-29 17:24:24 +0100

    update path to course materials zip

--- book/pre-rendering/zip-course-materials.R
+++ book/pre-rendering/zip-course-materials.R
@@ -1,6 +1,6 @@
 print("Zipping the course materials folder")
 
-zip(zipfile = "course-materials.zip", 
+zip(zipfile = "../course-materials.zip", 
     files = "../course-materials")
 
 print("Course materials zipped!")
